5fce1a299d48ca501ec5c8c0829ca97f14be602a
Nathan Teissier <nmteissier@gmail.com>
2023-04-07 11:29:33 +0200

    authProvider

--- src/main/java/com/luigivampa/auth/authentication/config/JwtAuthenticationUser.java
+++ src/main/java/com/luigivampa/auth/authentication/config/JwtAuthenticationUser.java
@@ -2,6 +2,11 @@ package com.luigivampa.auth.authentication.config;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class JwtAuthenticationUser extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -32,9 +38,20 @@ public class JwtAuthenticationUser extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
-        if(userEmail != null){
-            
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities());
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+
+            }
         }
+        filterChain.doFilter(request, response);
     }
 
 }
